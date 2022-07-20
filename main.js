1055896798c4b66eba98b3882941dfd2787a7b13
Ferru9 <ferferru2002@gmail.com>
2022-07-20 11:59:49 -0600

    Start

---
+++ main.js
@@ -0,0 +1,59 @@
+
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+
+navToggle.addEventListener('click', () => {
+     const visibility = primaryNav.getAttribute('data-visible');
+
+     if (visibility === "false") {
+          primaryNav.setAttribute("data-visible", true)
+          navToggle.setAttribute("aria-expanded", true)
+     } else if (visibility === "true"){
+          primaryNav.setAttribute("data-visible", false)
+          navToggle.setAttribute("aria-expanded", false)
+     }
+})
+
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleWindowResize);
+
+const spansSlow = document.querySelectorAll('.spanSlow');
+const spansFast = document.querySelectorAll('.spanFast');
+
+let width = window.innerWidth;
+
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width/2) - 1;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  spansSlow.forEach((span) => {
+    span.style.transform = `translate(${speedSlow}px)`;
+  });
+  spansFast.forEach((span) => {
+    span.style.transform = `translate(${speedFast}px)`
+  })
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
+
+
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+     
